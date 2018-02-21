9bd01e77acacbe4497059ae34725edef114c3d18
TinyA <1097912818@qq.com>
2018-02-21 20:47:28 +0800

    add connect

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,4 +1,7 @@
 import java.awt.*;
+import java.io.IOException;
+import java.net.*;
+
 import javax.swing.*;
 
 public class ChatClient extends JFrame {
@@ -6,6 +9,7 @@ public class ChatClient extends JFrame {
 	private static final long serialVersionUID = 1L;
 	JTextArea jtaMessage = new JTextArea();
 	JTextField jtf = new JTextField();
+	Socket s = null;
 	
 	public static void main(String args[]) {
 		new ChatClient();
@@ -16,6 +20,14 @@ public class ChatClient extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
+		
+		try {
+			s = new Socket("localhost", 1289);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		add(jtaMessage, BorderLayout.CENTER);
 		add(jtf, BorderLayout.SOUTH);
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,9 +1,14 @@
+import java.io.*;
+import java.net.*;
 import javax.swing.*;
 
 public class ChatServer extends JFrame {
 	
 	public static final long serialVersionUID = 1L;
 	JTextArea jtaMessage = new JTextArea();
+	ServerSocket ss = null;
+	Socket s = null;
+	boolean bConnect = false;
 	
 	public static void main(String args[]) {
 		new ChatServer();
@@ -14,9 +19,25 @@ public class ChatServer extends JFrame {
 //		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		
 		add(jtaMessage);
 		setVisible(true);
+		
+		try {
+			ss = new ServerSocket(1289);
+			bConnect = true;
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		while(bConnect) {
+			try {
+				s = ss.accept();
+System.out.println("A client connect");
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 	
 }
